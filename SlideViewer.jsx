4f8e1dfc5a1b37d9f287f6f53f1b2490baf286bc
englishbryan <english.bryan@gmail.com>
2026-04-09 09:50:07 -0400

    Spell out Department in title slide

--- SlideViewer.jsx
+++ SlideViewer.jsx
@@ -131,7 +131,7 @@ const TitleSlide = () => (
         <h1 className="text-5xl md:text-7xl font-extrabold tracking-tight mb-6 leading-tight text-white">
             Opioid Use Disorder <br />
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500">
-                in the Emergency Dept
+                in the Emergency Department
             </span>
         </h1>
         <h2 className="text-xl md:text-3xl text-slate-400 font-light mb-12">
